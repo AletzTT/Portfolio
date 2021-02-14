18c06d07d81ab791fe958f7fb026bd56bca64e69
AlexTT <aletz1305@gmail.com>
2021-02-13 18:50:25 -0600

    Primera Version

---
+++ main.js
@@ -0,0 +1,24 @@
+//MODALS
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img_id = "kb1";
+//var img = document.getElementsById(i);
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+function showModal(elmnt){
+    modal.style.display = "block";
+    modalImg.src = elmnt.src;
+    captionText.innerHTML = elmnt.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
